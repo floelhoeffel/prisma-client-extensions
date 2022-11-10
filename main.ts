36ac3b1ded6b416f4ce83dd267ca39ec44316111
Frederik Lölhöffel <6864947+floelhoeffel@users.noreply.github.com>
2022-11-10 15:03:34 +0100

    Example for model / validate
    Co-authored-by: Sergey Tatarintsev <sergey@tatarintsev.me>

--- main.ts
+++ main.ts
@@ -1,35 +1,58 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma, User } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const Joi = require("joi");
+const joiSchema = Joi.object({
+  age: Joi.number().integer().min(0).max(120).required(),
+  email: Joi.string().required()
+});
+
+const prisma = new PrismaClient();
+
+
+const xprisma = prisma.$extends({
+  model: {
+    user: {
+      validate(data: unknown): data is User  {
+
+        const result = joiSchema.validate(data);
+        return !result.error
+      },
+    },
+  },
+});
 
 async function main() {
-  console.log('Gude')
+  console.log("Client Extensions");
 
-
-  const xprisma = prisma.$extends({
-    model: {
-      user: {
-        async signUp(email: string) {
-          await prisma.user.create({
-            data: {
-                email: email
-            }
-          })
-        },
-      },
-    }
-  })
+  await signup({age: 150, email:"hello@world.com"})
 
 
 
 }
 
+async function signup(data:unknown) {
+
+ 
+
+    const userData = data
+    if (xprisma.user.validate(userData)) {
+      console.log("Valid!");
+      await xprisma.user.create({ data: userData })
+    } else {
+      // not valid
+      //xprisma.user.create({ data: userData })
+      console.log('not valid, hence no user created')
+    }
+ 
+
+}
+
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });

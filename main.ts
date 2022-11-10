138b720bdf79c6db3bfbd1c0075449c27cf92c13
Frederik Lölhöffel <6864947+floelhoeffel@users.noreply.github.com>
2022-11-10 16:10:04 +0100

    Added comments

--- main.ts
+++ main.ts
@@ -1,50 +1,54 @@
 import { PrismaClient, Prisma, User } from "@prisma/client";
 
+// Validation library
 const Joi = require("joi");
 const joiSchema = Joi.object({
+  // We set a range for the age integer and we validate email adress.
   age: Joi.number().integer().min(0).max(120).required(),
-  email: Joi.string().required()
+  email: Joi.string().email().required(),
 });
 
+// Prisma client
 const prisma = new PrismaClient();
 
-
-const xprisma = prisma.$extends({
+// Extended Prisma Client which only adds `person` if the age and email are valid
+const prismaWithCustomValidation = prisma.$extends({
+  // Customizing models works at runtime
   model: {
     user: {
-      validate(data: unknown): data is User  {
-
+      // Create a custom method `validate` on user models. Returns true if data is valid.
+      validate(data: unknown): data is User {
         const result = joiSchema.validate(data);
-        return !result.error
+        return !result.error;
       },
     },
   },
 });
 
 async function main() {
-  console.log("Client Extensions");
+  console.log("Client Extensions Demo");
 
-  await signup({age: 150, email:"hello@world.com"})
+  // Invalid user signup
+  await signup({ age: 150, email: "foo@bar.com" });
 
+  // Invalid user signup
+  await signup({ age: 50, email: "foobar.com" });
 
-
+  // Valid user signup
+  await signup({ age: 50, email: "foo@bar.com" });
 }
 
-async function signup(data:unknown) {
-
- 
-
-    const userData = data
-    if (xprisma.user.validate(userData)) {
-      console.log("Valid!");
-      await xprisma.user.create({ data: userData })
-    } else {
-      // not valid
-      //xprisma.user.create({ data: userData })
-      console.log('not valid, hence no user created')
-    }
- 
-
+async function signup(data: unknown) {
+  const userData = data;
+  if (prismaWithCustomValidation.user.validate(userData)) {
+    console.log("Valid!");
+    const user = await prismaWithCustomValidation.user.create({
+      data: userData,
+    });
+  } else {
+    // not valid
+    console.log("not valid, hence no user created");
+  }
 }
 
 main()
@@ -56,3 +60,42 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
+
+const prismaWithCustomResultAndQuery = prisma.$extends({
+  // Customizing results only works on a type level for you to try, doesn't work at runtime yet!
+  result: {
+    user: {
+      needs: {
+        fullName: { firstName: true, lastName: true },
+      },
+      fields: {
+        fullName(user) {
+          return `${user.firstName} ${user.lastName}`;
+        },
+      },
+    },
+  },
+  // Customizing queries only works on a type level for you to try, doesn't work at runtime yet!
+  query: {
+    user: {
+      async create({ args, data }) {
+        const result = joiSchema.validate(data);
+        if (result.error) {
+          throw result.error;
+        }
+        return data;
+      },
+    },
+  },
+});
+
+
+// This breaks at runtime and is hence commented out. But it shows the auto complete for the virtual field on `user`
+// const frederik = await prismaWithCustomResultAndQuery.user.findUnique({
+//   where: {
+//     id: 1,
+//   },
+// });
+// if (frederik) {
+//   console.log(frederik.fullName);
+// }
